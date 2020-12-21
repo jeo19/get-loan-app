1f3649d06806eedfeab286067c8c7b7b07cb082e
jeo19 <terrakkk@outlook.com>
2020-12-21 10:40:14 +0800

    feat:Add scope & closures step 1

--- scripts/obj.js
+++ scripts/obj.js
@@ -1,24 +1,9 @@
-var car={
-    make:"volvo",
-    speed:150,
-    engine:{
-        size:2.0,
-        make:"bmw",
-        fuel:"petrol",
-        pistons:[{maker:"BMW1"},{maker:"BMW2"}]
-    },
-    driven:function(){
-        return "drive";
+var engine={
+    maker:"Ford",
+    headGasket:{
+        pots:[
+            "pistons1",
+            "pistons2"
+        ]
     }
-};
-var array=[
-    "string",
-    100,
-    ["embed",200],
-    {car:"ford"},
-    function(){ return "drive";}
-]
-function name(fullname){
-    return fullname.firstname+fullname.lastname;
 }
-console.log(name({firstname:"jeo",lastname:"david"}));
