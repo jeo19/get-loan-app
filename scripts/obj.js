a95cd905071311f06443541929352c26ad9cc3da
jeo19 <terrakkk@outlook.com>
2020-12-21 23:33:38 +0800

    feat:Add scope & closures step 2

--- scripts/obj.js
+++ scripts/obj.js
@@ -1,9 +1,17 @@
-var engine={
-    maker:"Ford",
-    headGasket:{
-        pots:[
-            "pistons1",
-            "pistons2"
-        ]
-    }
-}
+console.log(this);
+var object={
+    prop:this,
+    method:function (){return this;}
+};
+
+var array=[
+    this,
+    function(){return this;}
+];
+
+function global(){
+    return this;
+};
+
+global.call(object);
+
