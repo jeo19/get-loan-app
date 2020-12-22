0a45366767313fed207bd0d963fc09a6084101fa
jeo19 <terrakkk@outlook.com>
2020-12-22 11:38:33 +0800

    add:Implicit Binding for the this keyword in Javascript step 2

--- scripts/obj.js
+++ scripts/obj.js
@@ -2,8 +2,16 @@ const user={
     name:"Tyler",
     age:27,
     great(){
-        console.log(`Hello, Myname is ${this.name}`)
+        console.log(`Hello, Myname is ${this.name}`);
+    },
+    mother:{
+        name:"Stacy",
+        age:40,
+        great(){
+            console.log(`Hello, ${user.name}'s mother is ${this.name}`);
+        }
     }
 }
 user.great();
+user.mother.great();
 
